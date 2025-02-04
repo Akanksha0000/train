a6f21e001fe74955fe530edf004bdf98970d1265
Akanksha0000 <akankshaubale68@gmail.com>
2025-02-04 12:24:08 +0530

    add the baseURL in the digitraffic_service.dart file

--- lib/services/digitraffic_service.dart
+++ lib/services/digitraffic_service.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
-
 import '../models/train.dart';
 
 class DigitrafficService {
@@ -21,18 +19,20 @@ class DigitrafficService {
     }
   }
 
-  Future<TrainDetails> getTrainDetails(String trainNumber) async {
+  Future<List<TrainDetails>> getTrainDetails(int trainNumber) async {
     final response = await http.get(
-      Uri.parse('$baseUrl/trains/$trainNumber'),
+      Uri.parse('$baseUrl/trains/latest/$trainNumber'),
       headers: {'Accept': 'application/json'},
     );
 
     if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-      if (data.isNotEmpty) {
-        return TrainDetails.fromJson(data.first);
-      }
-      throw Exception('Train not found');
+      final List data = json.decode(response.body);
+      if (data.isEmpty) return [];
+      
+      final timeTableRows = data[0]['timeTableRows'] as List;
+      return timeTableRows
+          .map((json) => TrainDetails.fromJson(json))
+          .toList();
     } else {
       throw Exception('Failed to load train details');
     }
